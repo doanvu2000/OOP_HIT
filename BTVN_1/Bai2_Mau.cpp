6276a46ed7bc692b981674f0e26978702a2ee1df
Vũ Văn Doan <55150896+doanvu2000@users.noreply.github.com>
2020-07-23 22:31:28 +0700

    Update Bai2_Mau.cpp
    update comment

--- BTVN_1/Bai2_Mau.cpp
+++ BTVN_1/Bai2_Mau.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 class SinhVien{
 	public:
-		// cac thuoc tinh
+		// các thuộc tính
 		string maSV, hoTen;
 		int tuoi;
 		float diem;
-		//phuong thuc xuat()
+		//phương thức xuat()
 		void xuat(){
 			cout<<"Thong tin sv: "<<endl;
 			cout<<"MaSV: "<<maSV<<endl;
@@ -17,17 +17,17 @@ class SinhVien{
 };
 int main(){
 	SinhVien a,b;
-	//gan thong tin SinhVien a
+	//gán thông tin SinhVien a
 	a.maSV = "123";
 	a.hoTen = "sinh vien A";
 	a.tuoi = 19;
 	a.diem = 9.5;
-	//gan thong tin SinhVien b
+	//gán thông tin SinhVien b
 	b.maSV = "567";
 	b.hoTen = "sinh vien B";
 	b.tuoi = 19;
 	b.diem = 8.5;
-	//hien thi thong tin 2 sinh vien
+	// hiển thị thông tin 2 2 đối tượng
 	a.xuat();
 	b.xuat();
 	return 0;
